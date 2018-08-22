997f15f8da1122b03da4c5dc3352a1686574c783
zhuyaokunll@foxmail.com <z28Y07k94>
2018-08-22 08:47:57 +0800

    今日头条2018校招算法方向（第一批）第二题-更新注释

--- src/MaxPoint.java
+++ src/MaxPoint.java
@@ -3,6 +3,29 @@ import java.util.*;
 /**
  * @author zhuyaokunll@foxmail.com
  * @date 2018/8/21 19:48
+ *
+ * 解题思路：
+ *       1、将点存为x, y ; 注意最后的输出是按照x由小到大，y由大到小排列
+ *       2、将点按照：优先 y 升序排列，x 降序排列
+ *       3、从后向前打印；先打印最后一个，并用max存储最后一个的x，然后向上遍历，满足x>max的就打印，并且更新max的值
+ * eg：
+ *       输入：
+ *       5
+ *       1 2
+ *       4 6
+ *       5 3
+ *       7 5
+ *       9 0
+ *       排序后：
+ *       9 0
+ *       1 2
+ *       5 3
+ *       7 5
+ *       4 6
+ *       期望输出：
+ *       4 6
+ *       7 5
+ *       9 0
  */
 public class MaxPoint {
     public static void main(String[] args) {
@@ -16,6 +39,8 @@ public class MaxPoint {
             tempList[1] = Integer.parseInt(temp[1]);
             weizhi.add(tempList);
         }
+        //将数组按照o1[1]优先升序排列，o1[0]次降序排列
+        //若o1[0]-o2[0]>0，则将o1[0]和o2[0]交换
         Collections.sort(weizhi, (o1, o2) -> o1[1]==o2[1]?o1[0]-o2[0]:o2[1]-o1[1]);
         System.out.println(weizhi.get(0)[0]+" "+weizhi.get(0)[1]);
         int max = weizhi.get(0)[0];
