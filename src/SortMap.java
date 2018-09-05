57a92244bfbb091982984ae1b744e875f4f86e5e
zhuyaokunll@foxmail.com <z28Y07k94>
2018-09-05 16:40:15 +0800

    添加一种只对hashmap的key进行排序的方法；简化使用set进行排序的方法，改为使用list保存map的entryset，对list进行排序就行

--- src/SortMap.java
+++ src/SortMap.java
@@ -12,18 +12,24 @@ public class SortMap {
         hashMap.put("z",102);
         hashMap.put("b",101);
         hashMap.put("c",5);
-        Set<Map.Entry<String,Integer>> set = new TreeSet<>(
-                (a,b) -> {
-                    if(a.getValue()==b.getValue())
-                        return a.getKey().compareTo(b.getKey());
-                    else
-                        return b.getValue().compareTo(a.getValue());
-                });
-        for(Map.Entry<String,Integer> one:hashMap.entrySet())
-        {
-            set.add(one);
-        }
+        //测试使用set和collections的sort方法进行排序，使用一个数据结构将hashmap的keyset拿出来，对它进行排序，最后遍历map的时候按照keyset的顺序来遍历就行
+        List<String> mapKey = new ArrayList<>(hashMap.keySet());
+        Collections.sort(mapKey);
         System.out.println();
+
+        //测试使用set对hashMap进行排序，具体是将hashmap中的每一个entry拿出来放到list中，使用Collections.sort对list进行排序
+        List<Map.Entry<String,Integer>> set = new ArrayList<Map.Entry<String,Integer>>(hashMap.entrySet());
+        Collections.sort(set,(a,b) -> {
+            //主排序按照map中的值降序排序
+            //次排序按照map的键值升序排序
+            if(a.getValue()==b.getValue())
+                return a.getKey().compareTo(b.getKey());
+            else
+                return b.getValue().compareTo(a.getValue());
+        });
+        System.out.println();
+
+        //测试使用TreeMap的key自然升序排列
         TreeMap<String,Integer> treeMap = new TreeMap<>();
         treeMap.put("a",12);
         treeMap.put("va",12);
